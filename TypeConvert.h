ffb69bbdfd924ff6e8618d57ed8b88f008a83450
zhenkas <jenkas@gmail.com>
2014-12-20 20:49:23 +0200

    TypeConvert works with Managed and Native type

--- TypeConvert.h
+++ TypeConvert.h
@@ -29,10 +29,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #pragma once
-#include "pp_foreach.h"
-#include "SafeObject.h"
 
 #ifdef _MANAGED
+#include <gcroot.h>
 #define REF_SYMBOL %
 #else
 #define REF_SYMBOL &
@@ -45,7 +44,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 		namespace TypeConvert
 		{
-			inline void ConvertImplementation(TDestination REF_SYMBOL dst, const TSource REF_SYMBOL src)
+			inline void ConvertImplementation(TDestination & dst, const TSource & src)
 			{
 				dst = src;
 			}
@@ -56,7 +55,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 		struct TDestination/TSource
 		{
 		public:
-			inline static void ConvertImplementation(TDestination REF_SYMBOL dst, const TSource REF_SYMBOL src)
+			inline static void ConvertImplementation(TDestination & dst, const TSource & src)
 			{
 				dst = src;
 			}
@@ -71,105 +70,233 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace TypeConvert
 {
+	using namespace Traits;
+
 	template<typename T>
-	struct ConvertCallerFromType
+	struct ConvertCallerFromTypeN_N
 	{
+		typedef typename std::remove_reference<T>::type type;
 		template<typename TDst, typename TSrc>
-		static inline void Convert(TDst REF_SYMBOL dst, const TSrc REF_SYMBOL src) 
+		static inline void Convert(TDst & dst, TSrc && src)
 		{
-			T::ConvertImplementation(dst, src);		
+			type::ConvertImplementation(dst, std::forward<TSrc>(src));
 		}
 	};
-
-	struct ConvertCallerFromNamespace
+#ifdef _MANAGED
+	template<typename T>
+	struct ConvertCallerFromTypeM_N
+	{
+		typedef typename std::remove_reference<T>::type type;
+		template<typename TDst, typename TSrc>
+		static inline void Convert(TDst % dst, TSrc && src)
+		{
+			type::ConvertImplementation(dst, std::forward<TSrc>(src));
+		}
+	};
+	template<typename T>
+	struct ConvertCallerFromTypeN_M
+	{
+		typedef typename std::remove_reference<T>::type type;
+		template<typename TDst, typename TSrc>
+		static inline void Convert(TDst & dst, TSrc % src)
+		{
+			type::ConvertImplementation(dst, src);
+		}
+	};
+#endif
+	
+	struct ConvertCallerFromNamespaceN_N
 	{
 		template<typename TDst, typename TSrc>
-		static inline void Convert(TDst REF_SYMBOL dst, const TSrc REF_SYMBOL src)
+		static inline void Convert(TDst & dst, TSrc && src)
+		{
+			TypeConvert::ConvertImplementation(dst, std::forward<TSrc>(src));
+		}
+	};
+#ifdef _MANAGED
+	struct ConvertCallerFromNamespaceM_N
+	{
+		template<typename TDst, typename TSrc>
+		static inline void Convert(TDst % dst, TSrc && src)
+		{
+			TypeConvert::ConvertImplementation(dst, std::forward<TSrc>(src));
+		}
+	};
+	struct ConvertCallerFromNamespaceN_M
+	{
+		template<typename TDst, typename TSrc>
+		static inline void Convert(TDst & dst, TSrc % src)
 		{
 			TypeConvert::ConvertImplementation(dst, src);
 		}
 	};
+	struct ConvertCallerFromNamespaceM_M
+	{
+		template<typename TDst, typename TSrc>
+		static inline void Convert(TDst % dst, TSrc % src)
+		{
+			TypeConvert::ConvertImplementation(dst, src);
+		}
+	}; 
+#endif
 
- 	template<typename TDst, typename TSrc>
-	auto GetConvertCaller(TDst REF_SYMBOL dst, const TSrc REF_SYMBOL src, decltype(((TDst*)0)->ConvertImplementation(dst, src)) *)-> ConvertCallerFromType<TDst>;
+
+#define CHECK_DST	TraceType<decltype(((std::remove_reference<TDst>::type*)0)->ConvertImplementation(dst, std::forward<TSrc>(src)))>
+#define CHECK_DST2	TraceType<decltype(((std::remove_reference<TDst>::type*)0)->ConvertImplementation(dst, src))>
+#define CHECK_SRC	TraceType<decltype(((std::remove_reference<TSrc>::type*)0)->ConvertImplementation(dst, std::forward<TSrc>(src)))>
+#define CHECK_SRC2	TraceType<decltype(((std::remove_reference<TSrc>::type*)0)->ConvertImplementation(dst, src))>
+
 	template<typename TDst, typename TSrc>
-	auto GetConvertCaller(TDst REF_SYMBOL dst, const TSrc REF_SYMBOL src, decltype(((TSrc*)0)->ConvertImplementation(dst, src)) *)->ConvertCallerFromType<TSrc>;
+	auto GetConvertCallerN_N_2(TDst & dst, TSrc && src, typename CHECK_DST::type *)-> ConvertCallerFromTypeN_N<TDst>;
 	template<typename TDst, typename TSrc>
-	auto GetConvertCaller(TDst REF_SYMBOL dst, const TSrc REF_SYMBOL src, ...)->ConvertCallerFromNamespace;
+	auto GetConvertCallerN_N_2(TDst & dst, TSrc && src, typename CHECK_SRC::type *)->ConvertCallerFromTypeN_N<TSrc>;
+#ifdef _MANAGED
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerN_M_2(TDst & dst, TSrc % src, typename CHECK_DST2::type *)-> ConvertCallerFromTypeN_M<TDst>;
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerM_N_2(TDst % dst, TSrc && src, typename CHECK_SRC::type *)-> ConvertCallerFromTypeM_N<TSrc>;	
+#endif
+
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerN_N(TDst & dst, TSrc && src, ...)->ConvertCallerFromNamespaceN_N;
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerN_N(TDst & dst, TSrc && src, decltype(GetConvertCallerN_N_2(dst, std::forward<TSrc>(src), 0))*) -> decltype(GetConvertCallerN_N_2(dst, std::forward<TSrc>(src), 0));
+#ifdef _MANAGED
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerN_M(TDst & dst, TSrc % src, ...)-> ConvertCallerFromNamespaceN_M;
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerN_M(TDst & dst, TSrc % src, decltype(GetConvertCallerN_M_2(dst, src, 0))*) -> decltype(GetConvertCallerN_M_2(dst, src, 0));
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerM_N(TDst % dst, TSrc && src, ...)->ConvertCallerFromNamespaceM_N;
+	template<typename TDst, typename TSrc>
+	auto GetConvertCallerM_N(TDst % dst, TSrc && src, decltype(GetConvertCallerM_N_2(dst, std::forward<TSrc>(src), 0))*) -> decltype(GetConvertCallerM_N_2(dst, std::forward<TSrc>(src), 0));
+#endif
+
+//BASIC
+
+	template<typename TDst, typename TSrc>
+	auto GetConvertCaller(typename std::enable_if<TestIsNative<TDst>::value, TDst>::type & dst, typename std::enable_if<TestIsNative<TSrc>::value, TSrc>::type && src)-> decltype(GetConvertCallerN_N(dst, std::forward<TSrc>(src), 0));
+#ifdef _MANAGED
+	template<typename TDst, typename TSrc>
+	auto GetConvertCaller(typename std::enable_if<TestIsNative<TDst>::value, TDst>::type & dst, typename std::enable_if<TestIsManaged<TSrc>::value, TSrc>::type % src)-> decltype(GetConvertCallerN_M(dst, src, 0));
+	template<typename TDst, typename TSrc>
+	auto GetConvertCaller(typename std::enable_if<TestIsManaged<TDst>::value, TDst>::type % dst, typename std::enable_if<TestIsNative<TSrc>::value, TSrc>::type && src)-> decltype(GetConvertCallerM_N(dst, std::forward<TSrc>(src), 0));
+	template<typename TDst, typename TSrc>
+	auto GetConvertCaller(typename std::enable_if<TestIsManaged<TDst>::value, TDst>::type % dst, typename std::enable_if<TestIsManaged<TSrc>::value, TSrc>::type % src)-> ConvertCallerFromNamespaceM_M;
+#endif
 
 	template<typename TSrc>
-	struct ConvertSrcHolder
+	struct ConvertSrcHolderNative
 	{
-		inline ConvertSrcHolder(const TSrc & src):m_src(src) {}
+		typedef typename std::add_lvalue_reference<TSrc>::type TSrcRef;
+		template<typename TSrc2>
+		inline ConvertSrcHolderNative(TSrc2 && src) : m_src(src) {}
 		template<typename TDst>
-		inline operator const TDst () const
+		inline operator TDst () const
 		{
-			PP_REMOVE_MANAGED_CONST(std::remove_const<std::remove_reference<TDst>::type>::type()) dst;
-			typedef decltype(GetConvertCaller(dst, m_src, 0)) TConvertCaller;
+			typedef PP_REMOVE_MANAGED_CONST(std::remove_const<std::remove_reference<TDst>::type>::type()) TDstClean;
+			TDstClean dst;
+			typedef decltype(GetConvertCaller<TDstClean, TSrcRef>(dst, m_src)) TConvertCaller;
 			TConvertCaller::Convert(dst, m_src);
 			return dst;
 		}
 		template<typename TDst>
 		inline void ConvertTo(TDst & dst) const
 		{
-			typedef decltype(GetConvertCaller(dst, m_src, 0)) TConvertCaller;
+			typedef decltype(GetConvertCaller<TDst, TSrcRef>(dst, m_src)) TConvertCaller;
 			TConvertCaller::Convert(dst, m_src);
 		}
-		const TSrc & m_src;
+		TSrcRef m_src;
 	};
 
 #ifdef _MANAGED
 	template<typename TSrc>
-	value struct ConvertSrcHolderManaged
+	struct ConvertSrcHolderManaged
 	{
-		inline ConvertSrcHolderManaged(const TSrc ^ const % src):	m_src(src) {}
-		const TSrc ^ m_src;
+		inline ConvertSrcHolderManaged(TSrc % src) : m_src(src){}
+		template<typename TDst>
+		inline operator TDst () const
+		{
+			typedef PP_REMOVE_MANAGED_CONST(std::remove_const<std::remove_reference<TDst>::type>::type()) TDstClean;
+			TDstClean dst;
+			typedef decltype(GetConvertCaller<TDstClean, TSrc>(dst, m_src.operator->())) TConvertCaller;
+			TConvertCaller::Convert(dst, m_src.operator->());
+			return dst;
+		}
+		template<typename TDst>
+		inline void ConvertTo(TDst & dst) const
+		{
+			typedef decltype(GetConvertCaller<TDst, TSrc>(dst, m_src.operator->())) TConvertCaller;
+			TConvertCaller::Convert(dst, m_src.operator->());
+		}
+		gcroot<TSrc> m_src;
 	};
+
 	template<typename TSrc>
-	inline auto ConvertWithHolder(const TSrc ^ const % src) -> ConvertSrcHolder<ConvertSrcHolderManaged<TSrc>>
+	auto Convert(TSrc % src) -> typename std::enable_if<TestIsManaged<TSrc>::value, ConvertSrcHolderManaged<TSrc>>::type
 	{
-		return ConvertSrcHolder<ConvertSrcHolderManaged<TSrc>>(ConvertSrcHolderManaged<TSrc>(src));
+		return ConvertSrcHolderManaged<TSrc>(src);
+	}
+
+	template<typename TDst, typename TSrc>
+	inline auto Convert(TSrc src) -> typename std::enable_if<TestIsManaged<TSrc>::value, TDst>::type
+	{
+		typedef PP_REMOVE_MANAGED_CONST(std::remove_const<std::remove_reference<TDst>::type>::type()) TDstClean;
+		TDstClean dst;
+		typedef decltype(GetConvertCaller<TDstClean, TSrc>(dst, src)) TConvertCaller;
+		TConvertCaller::Convert(dst, src);
+		return dst;
+	}
+
+#endif
+
+	template<typename TSrc>
+	auto Convert(TSrc && src) -> typename std::enable_if<TestIsNative<TSrc>::value, ConvertSrcHolderNative<TSrc>>::type
+	{
+		return ConvertSrcHolderNative<TSrc>(std::forward<TSrc>(src));
+	}
+
+
+	template<typename TDst, typename TSrc>
+	inline auto Convert(TSrc && src) -> typename std::enable_if<TestIsNative<TSrc>::value, TDst>::type
+	{
+		typedef PP_REMOVE_MANAGED_CONST(std::remove_const<std::remove_reference<TDst>::type>::type()) TDstClean;
+		TDstClean dst;
+		typedef decltype(GetConvertCaller<TDstClean, TSrc>(dst, std::forward<TSrc>(src))) TConvertCaller;
+		TConvertCaller::Convert(dst, std::forward<TSrc>(src));
+		return dst;
+	}
+
+	template<typename TDst, typename TSrc>
+	inline auto Convert(TDst & dst, TSrc && src) -> typename std::enable_if<BothTrue<TestIsNative<TDst>::value, TestIsNative<TSrc>::value>::value, TDst>::type &
+	{
+		typedef decltype(GetConvertCaller<TDst, TSrc>(dst, std::forward<TSrc>(src))) TConvertCaller;
+		TConvertCaller::Convert(dst, std::forward<TSrc>(src));
+		return dst;
+	}
+#ifdef _MANAGED
+	template<typename TDst, typename TSrc>
+	inline auto Convert(TDst % dst, TSrc && src) -> typename std::enable_if<BothTrue<TestIsManaged<TDst>::value, TestIsNative<TSrc>::value>::value, TDst>::type %
+	{
+		typedef decltype(GetConvertCaller<TDst, TSrc>(dst, std::forward<TSrc>(src))) TConvertCaller;
+		TConvertCaller::Convert(dst, std::forward<TSrc>(src));
+		return dst;	
 	}
 	template<typename TDst, typename TSrc>
-	inline void ConvertImplementation(TDst % dst, const ConvertSrcHolderManaged<TSrc> % src)
+	inline auto Convert(TDst & dst, TSrc src) -> typename std::enable_if<BothTrue<TestIsNative<TDst>::value, TestIsManaged<TSrc>::value>::value, TDst>::type &
 	{
-		Convert(dst, src.m_src);
+		typedef decltype(GetConvertCaller<TDst, TSrc>(dst, src)) TConvertCaller;
+		TConvertCaller::Convert(dst, src);
+		return dst;	
+	}
+	template<typename TDst, typename TSrc>
+	inline auto Convert(TDst % dst, TSrc src) -> typename std::enable_if<BothTrue<TestIsManaged<TDst>::value, TestIsManaged<TSrc>::value>::value, TDst>::type %
+	{
+		typedef decltype(GetConvertCaller<TDst, TSrc>(dst, src)) TConvertCaller;
+		TConvertCaller::Convert(dst, src);
+		return dst;
 	}
 #endif
-	template<typename TSrc>
-	inline auto ConvertWithHolder(const TSrc & src) -> ConvertSrcHolder<TSrc>
-	{
-		return ConvertSrcHolder<TSrc>(src);
-	}
-	template<typename TSrc>
-	inline auto Convert(const TSrc & src) -> decltype(ConvertWithHolder(src))
-	{
-		return ConvertWithHolder(src);
-	}
-
-	template<typename TDst, typename TSrc>
-	inline TDst Convert(const TSrc REF_SYMBOL src)
-	{
-		PP_REMOVE_MANAGED_CONST(std::remove_const<std::remove_reference<TDst>::type>::type()) dst;
-		typedef decltype(GetConvertCaller(dst, src, 0)) TConvertCaller;
-		TConvertCaller::Convert(dst, src);		
-		return dst;
-	}
-	template<typename TDst, typename TSrc>
-	inline auto Convert(TDst REF_SYMBOL dst, const TSrc REF_SYMBOL src) -> TDst REF_SYMBOL
-	{
-		typedef decltype(GetConvertCaller(dst, src, 0)) TConvertCaller;
-		TConvertCaller::Convert(dst, src);
-		return dst;
-	}
-
-	template<typename TDst, typename TSrc>
-	inline auto operator << (TDst REF_SYMBOL dst, const TSrc REF_SYMBOL src) -> TDst REF_SYMBOL
-	{
-		typedef decltype(GetConvertCaller(dst, src, 0)) TConvertCaller;
-		TConvertCaller::Convert(dst, src);
-		return dst;
-	};
 }
 
 #define CONVERT_SINGLE_MEMBER_NATIVE(param, v) TypeConvert::Convert(rdst.v, src->v);
@@ -219,5 +346,3 @@ static inline void ConvertImplementation(TDst ^% dst, const TSrc & src)\
 #else 
 #define CONVERT_MEMBERS(...)
 #endif
-
-
